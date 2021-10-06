3a8b7ef52b44db7c14611e3315e8e19d3edab062
Kerav Nahar <74814108+KeravNahar@users.noreply.github.com>
2021-10-06 11:25:13 +0530

    Update non_veg.dart

--- lib/screens/categories/non_veg.dart
+++ lib/screens/categories/non_veg.dart
@@ -72,7 +72,7 @@ class NonVegPage extends StatelessWidget {
                 children: [
                   SizedBox(height: 35,),
                   Padding(
-                    padding: const EdgeInsets.only(right: 225),
+                    padding: const EdgeInsets.only(left: 30),
                     child: Text("Vendors Near You",style: TextStyle(fontSize: 15,color: Colors.teal[900],fontWeight: FontWeight.bold),textAlign: TextAlign.left,),
                   ),
                   SizedBox(height: 10,),
